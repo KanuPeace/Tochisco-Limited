b31682a16f0e9b93ad55e55c06613df3c50dbca8
Kanu Peace <kanupeace85@gmail.com>
2022-01-05 12:28:38 +0100

    title change

--- resources/views/app/layouts.blade.php
+++ resources/views/app/layouts.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="{{ asset($public.'/css/app.css') }}" rel="stylesheet">
 
-    <title>Tochisco Limited</title>
+    <title>Tochisco</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
--- resources/views/web/pages/layouts/app.blade.php
+++ resources/views/web/pages/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="keywords" content="Aler, unica, creative, html">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>TOCHISCO LIMITED</title>
+        <title>Tochisco</title>
     
         <!-- Google Font -->
         @include("web.pages.layouts.includes.header")
