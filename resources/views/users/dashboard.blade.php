65dcf0141a81ca7902a01d2bf98a777128d148e1
Kanu Peace <kanupeace85@gmail.com>
2022-01-31 14:43:28 +0100

    users dashboard views

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -87,7 +87,193 @@
                         </div>
                     </div>
                 </section> --}}
-               
+                <div class="container">
+                  
+                    {{-- <div class="alert alert-danger mb-4">You are currently not subscribled to any plan, kindly
+                        <a href="" class="text-success">click here to subscribe....</a>
+                    </div> --}}
+                    
+                
+                    <div class="row">
+                        <div class="col-md-auto col-12 text-center">
+                            <img src="" alt="avatar" class="img-fluid profile mt-5">
+                            <div><a href="" class="badge badge-pill badge-danger">Edit</a></div>
+                        </div>
+                        <div class="col-md-auto col-12 text-center">
+                            <h4 class="mt-3">Welcome </h4>
+                        </div>
+                    </div>
+                    <div class="row ">
+                        {{-- <div class="col-md-5">
+                                @include("dashboards.user.profile_card")
+                            </div> --}}
+                
+                        <div class="col-md-6">
+                            <div class="profile_item_card text-center mt-3">
+                                <p class="">Wallet Balance</p>
+                                <h2></h2>
+                                <div class="row mt-3 mb-5">
+                                    <div class="col-6">
+                                        <a href="" class="mybtn btn-success btn-lg p-3">Deposit</a>
+                                    </div>
+                                    <div class="col-6">
+                                        <a href="" class="mybtn btn-danger btn-lg p-3">Withdraw</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                
+                        <div class="col-md-6">
+                            <div class="row">
+                                <div class="col-md-6 mt-5">
+                                    <div class="profile_item_card text-center">
+                                        <div class="mb-2">Referral Earnings</div>
+                                        <h4></h4>
+                                        <div class="mb-4">
+                                            <form onsubmit="return confirm('Are you sure you want to transfer all funds to your main wallet?')" action="" method="post">@csrf
+                                                <span type="submit" class="badge badge-pill badge-success p-2" onclick="return $(this).parent().submit()">
+                                                    Transfer to
+                                                    Wallet</span>
+                                            </form>
+                
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 mt-5">
+                                    <div class="profile_item_card text-center">
+                                        <div class="mb-2">Non-referral Earnings</div>
+                                        <h4></h4>
+                                        <div class="mb-4">
+                                            <form onsubmit="return confirm('Are you sure you want to transfer all funds to your main wallet?')" action="" method="post">@csrf
+                                                <span type="submit" class="badge badge-pill badge-success p-2" onclick="return $(this).parent().submit()">
+                                                    Transfer to
+                                                    Wallet</span>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                
+                            </div>
+                
+                        </div>
+                
+                
+                    </div>
+                
+                    <div class="alert alert-success mt-5">
+                        Invite your friends to join and earn massively now 💰💰🤑🤑🤑🥳🥳🥳🥳!
+                
+                        <p>
+                            Reference Code: <a href="javascript:;;" class="text-primary" onclick="return copyToClipboard('')">
+                              
+                            </a>
+                
+                            <br>Or <br>
+                
+                            <a href="javascript:;;" class="text-primary" onclick="return copyToClipboard('')">
+                                Copy referral link
+                            </a>
+                        </p>
+                    </div>
+                
+                    @include("users.layouts.fragments.youtube_subscribe_message")
+                
+                    <div class="alert alert-info mt-4">
+                        <a href="" class="">Click here to buy COUPON CODES</a>
+                    </div>
+                
+                    <div class="alert alert-success mt-4">
+                        <a href="" class="">Click here to upgrade your plan</a>
+                    </div>
+                
+                
+                    <div class="form-row">
+                
+                        <div class="col-md-auto col-6">
+                            <div class="alert alert-dark mt-2">
+                                <link rel="shortcut icon" type="image/x-icon" href="">
+                                Sponsored <br> Posts <br>
+                                <a href="" class="btn-lg mybtn btn-success p-2 mt-3 mb-4 text-white ">View</a>
+                
+                            </div>
+                        </div>
+                
+                
+                        <div class="col-md-auto col-6">
+                            <div class="alert alert-info mt-2">
+                                My <br> Referrals <br>
+                                <a href="" class="btn-lg mybtn btn-success p-2 mt-3 mb-4 text-white ">View</a>
+                
+                            </div>
+                        </div>
+                
+                
+                        <div class="col-md-auto col-6">
+                            <div class="alert alert-danger mt-2">
+                                My <br> Withdrawals <br>
+                                <a href="" class="btn-lg mybtn btn-success p-2 mt-3 mb-4 text-white ">View</a>
+                
+                            </div>
+                        </div>
+                
+                        <div class="col-md-auto col-6">
+                            <div class="alert alert-warning mt-2">
+                                My <br> Transactions <br>
+                                <a href="" class="btn-lg mybtn btn-success p-2 mt-3 mb-4 text-white ">View</a>
+                
+                            </div>
+                        </div>
+                
+                
+                
+                    </div>
+                
+                    {{-- <h4 class="mt-5">Available Videos</h4>
+                        <div class="form-row">
+                
+                            <div class="col-md-auto col-12">
+                                <div class="alert alert-success mt-2">
+                                    Cryptocurrency Videos
+                                </div>
+                            </div>
+                
+                            <div class="col-md-auto col-12">
+                                <div class="alert alert-success mt-2">
+                                    Cryptocurrency Videos
+                                </div>
+                            </div>
+                            <div class="col-md-auto col-12">
+                                <div class="alert alert-success mt-2">
+                                    Cryptocurrency Videos
+                                </div>
+                            </div>
+                            <div class="col-md-auto col-12">
+                                <div class="alert alert-success mt-2">
+                                    Cryptocurrency Videos
+                                </div>
+                            </div>
+                
+                
+                        </div> --}}
+                
+                   
+                    <div class="">
+                        <div class="alert alert-success mt-2">
+                            <b> Play 2 Earn</b>
+                
+                            <p>Earn from activities like surveys and games directly into your account </p>
+                
+                            <form action="" method="POST">@csrf
+                                <button class="btn btn-danger btn-sm">Join now</button>
+                            </form>
+                
+                
+                        </div>
+                    </div>
+                 
+                
+                
+                </div>
             </div>
 
         </div>
