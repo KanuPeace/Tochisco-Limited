1e60df9e0d2a35732b8d643f5b95b850ef96abca
Kanu Peace <kanupeace85@gmail.com>
2022-03-01 14:20:22 +0100

    users profile

--- resources/views/users/profile/create.blade.php
+++ resources/views/users/profile/create.blade.php
@@ -1,36 +1,40 @@
 @extends('users.layouts.app')
 
 @section('content')
-<section class="content">
-    <div class="container-fluid">
-    @include('notifications.flash_messages')
-        <div class="row">
-            <div class="col-sm-12 col-md-12 col-lg-8 col-xl-8 bg-white p-5">
-                <form enctype="multipart/form-data" action="{{route('user.profile.store')}}" method="post">
-                    @csrf
-                    <label class="form-group" for="image">Add Profile Image</label>
-                    <div class="col-8">
-                        <input class=" col-8 form-group" type="file" name="avatar" id="avatar">
+    <div id="content" class="main-content">
+        <div class="layout-px-spacing">
+
+            <div class="row layout-spacing">
+
+                <!-- Content -->
+                <div class="col-xl-4 col-lg-12 col-md-5 col-sm-12 layout-top-spacing">
+                    @include('notifications.flash_messages')
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12 col-lg-8 col-xl-8 bg-white p-5">
+                            <form enctype="multipart/form-data" action="{{ route('user.profile.store') }}" method="post">
+                                @csrf
+                                <label class="form-group" for="image">Add Profile Image</label>
+                                <div class="col-8">
+                                    <input class=" col-32 form-group" type="file" name="avatar" id="avatar">
+                                </div>
+                                <div class="col-8">
+                                    <button class="btn btn-secondary text-white w-100">Save</button>
+                                </div>
+                            </form>
+                            <p><a href="{{ route('user.profile.index') }}">Back to profile</a></p>
+                        </div>
+
                     </div>
-                    <div class="col-8">
-                        <button class="btn btn-secondary text-white w-100">Save</button>
-                    </div>
-                </form>
-                <p><a href="{{route('user.profile.index')}}">Back to profile</a></p>
+                </div>
+                <!-- /.content-wrapper -->
+
+                <!-- Control Sidebar -->
+                <aside class="control-sidebar control-sidebar-dark">
+                    <!-- Control sidebar content goes here -->
+                </aside>
+                <!-- /.control-sidebar -->
             </div>
-
         </div>
+        <!-- ./wrapper -->
     </div>
-    <!-- /.content-wrapper -->
-
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
-    </div>
-    </div>
-    <!-- ./wrapper -->
-
-
     @endsection
--- resources/views/users/profile/index.blade.php
+++ resources/views/users/profile/index.blade.php
@@ -1,37 +1,41 @@
 @extends('users.layouts.app')
 
 @section('content')
-<section class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12 col-md-12 col-lg-4 col-xl-4 bg-white p-5">
-                <div style="font-size: bold;" class="mb-3">
-                   
+    <div id="content" class="main-content">
+        <div class="layout-px-spacing">
+
+            <div class="row layout-spacing">
+
+                <!-- Content -->
+                <div class="col-xl-4 col-lg-6 col-md-5 col-sm-12 layout-top-spacing">
+                    <div style="font-size: bold;" class="mb-3">
+
+                    </div>
+                    <div class="profile-image">
+                        <img style="width: 100%; height:40vh;" class=""
+                            src="{{ asset('profileImages/' . $profile->avatar) }}" alt="">
+                    </div>
+                    <div class="user_info mt-5">
+                        <div><b class="pr-3">Name:</b> <span><i>{{ auth()->user()->name }}</i></span></div>
+
+                        <div><b class="pr-3">Joined On:</b>
+                            <span><i>{{ auth()->user()->created_at->toDateString() }}</i></span></div>
+
+                    </div>
+                    <a href="{{ route('user.profile.create') }}" class=" mt-3 btn btn-secondary text-white w-100">Change
+                        Profile Image</a>
                 </div>
-                <div class="profile-image">
-                    <img style="width: 100%; height:40vh;" class="" src="{{asset('profileImages/' .  $profile->avatar)}}" alt="">
-                </div>
-              <div class="user_info mt-5">
-              <div><b class="pr-3">Name:</b>   <span><i>{{auth()->user()->name}}</i></span></div>  
-                
-              <div><b class="pr-3">Joined On:</b>   <span><i>{{auth()->user()->created_at->toDateString()}}</i></span></div> 
-              
-              </div>
-              <a href="{{route('user.profile.create')}}" class=" mt-3 btn btn-secondary text-white w-100">Change Profile Image</a>
+
             </div>
-
         </div>
-    </div>
-    <!-- /.content-wrapper -->
+        <!-- /.content-wrapper -->
 
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
     </div>
     </div>
     <!-- ./wrapper -->
-
-
-    @endsection
+@endsection
