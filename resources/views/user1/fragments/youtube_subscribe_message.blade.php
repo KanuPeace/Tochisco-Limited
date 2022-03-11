9410aa254c9aac783c0860700bc0fa6b857d4f2a
Kanu Peace <kanupeace85@gmail.com>
2022-03-11 16:57:55 +0100

    saving state

--- resources/views/user1/fragments/youtube_subscribe_message.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="alert alert-warning mt-5">
-    <div>Subscribe to our YouTube channel now to have access to the basic courses Online; <br>
-        Also know that you won't be able to withdraw if you don't. <br></div>
-    <a href="https://youtube.com/channel/UCXp0uqAQ8XD-iFnMuWnZGvg" target="_blank" class="btn-lg mybtn btn-success mt-3 mb-4 text-white ">
-        SUBSCRIBE NOW</a>
-</div>
