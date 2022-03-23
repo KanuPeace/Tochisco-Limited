20d3311ff0f59b40012a346962009ff9a7a30770
Kanu Peace <kanupeace85@gmail.com>
2022-03-23 16:42:35 +0100

    text

--- resources/views/web/welcome.blade.php
+++ resources/views/web/welcome.blade.php
@@ -705,15 +705,7 @@
                     </div>
                 </div>
             </div>
-            {{-- <div class="copyright-text">
-            <p>
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-               <script>
-                    document.write(new Date().getFullYear());
-                </script>
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-        </div> --}}
+          
         </div>
     </footer>
     <!-- Footer Section End -->
