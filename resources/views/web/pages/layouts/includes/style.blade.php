dff7d842f50f4d90252510d833918e55afa0e0f1
Kanu Peace <kanupeace85@gmail.com>
2022-03-30 17:17:50 +0100

    saving state

--- resources/views/web/pages/layouts/includes/style.blade.php
+++ resources/views/web/pages/layouts/includes/style.blade.php
@@ -24,7 +24,5 @@
 <link rel="stylesheet" href="web_assets/css/style.css" type="text/css">
 
 
-  <!-- fontawesome link -->
-
         <link rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
