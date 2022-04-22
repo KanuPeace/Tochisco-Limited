10b9e4cee6489d28ae24d8910e83e14ef9f8775d
Kanu Peace <kanupeace85@gmail.com>
2022-04-22 14:29:24 +0100

    prperty copyright

--- resources/views/web/pages/property/property.blade.php
+++ resources/views/web/pages/property/property.blade.php
@@ -369,11 +369,7 @@
                     </div>
                 </div>
             </div>
-            <div class="copyright-text">
-                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>, distributed by <a href="https://bootthemes.com/" target="_blank">BootThemes</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-            </div>
+           
         </div>
     </footer>
     @endsection
