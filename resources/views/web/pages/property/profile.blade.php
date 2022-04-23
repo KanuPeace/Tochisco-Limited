b123dfb1b68518ef473298aa3b9ccce9991b2faf
Kanu Peace <kanupeace85@gmail.com>
2022-04-23 18:17:54 +0100

    saving state

--- resources/views/web/pages/property/profile.blade.php
+++ resources/views/web/pages/property/profile.blade.php
@@ -3,8 +3,8 @@
 @section('content') --}}
 
 
-    <!-- Breadcrumb Section Begin -->
-    {{-- <section class="breadcrumb-section spad set-bg" data-setbg="web_assets/img/breadcrumb-bg.jpg" >
+<!-- Breadcrumb Section Begin -->
+{{-- <section class="breadcrumb-section spad set-bg" data-setbg="web_assets/img/breadcrumb-bg.jpg" >
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
@@ -20,7 +20,7 @@
             </div>
         </div>
     </section> --}}
-    <!--<section class="breadcrumb-section spad set-bg" data-setbg="img/breadcrumb-bg.jpg">
+<!--<section class="breadcrumb-section spad set-bg" data-setbg="img/breadcrumb-bg.jpg">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
@@ -36,10 +36,10 @@
             </div>
         </div>
     </section> -->
-    <!-- Breadcrumb Section End -->
+<!-- Breadcrumb Section End -->
 
-    <!-- Profile Section Begin -->
-    {{-- <section class="profile-section spad">
+<!-- Profile Section Begin -->
+{{-- <section class="profile-section spad">
         <div class="container">
             <div class="profile-agent-content">
                 <div class="row">
@@ -80,7 +80,7 @@
             </div>
         </div>
     </section> --}}
-    <!--<section class="profile-section spad">
+<!--<section class="profile-section spad">
         <div class="container">
             <div class="profile-agent-content">
                 <div class="row">
@@ -121,10 +121,10 @@
             </div>
         </div>
     </section> -->
-    <!-- Profile Section End -->
+<!-- Profile Section End -->
 
-    <!-- Property Section Begin -->
-    {{-- <section class="property-section profile-page spad">
+<!-- Property Section Begin -->
+{{-- <section class="property-section profile-page spad">
         <div class="container">
             <div class="row">
                 <div class="col-lg-4">
@@ -318,7 +318,7 @@
             </div>
         </div>
     </section> --}}
-    <!--<section class="property-section profile-page spad">
+<!--<section class="property-section profile-page spad">
         <div class="container">
             <div class="row">
                 <div class="col-lg-4">
@@ -512,10 +512,10 @@
             </div>
         </div>
     </section> -->
-    <!-- Property Section End -->
+<!-- Property Section End -->
 
-    <!-- Contact Section Begin -->
-    {{-- <section class="contact-section">
+<!-- Contact Section Begin -->
+{{-- <section class="contact-section">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
@@ -558,73 +558,80 @@
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d735515.5813275519!2d-80.41163541934742!3d43.93644386501528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882a55bbf3de23d7%3A0x3ada5af229b47375!2sMono%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sbd!4v1583262687289!5m2!1sen!2sbd" height="450" style="border:0;" allowfullscreen=""></iframe>
         </div>
     </section> --}}
-    <!-- Contact Section End -->
+<!-- Contact Section End -->
 
-    <!-- Footer Section Begin -->
-    <footer class="footer-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="fs-about">
-                        <div class="fs-logo">
-                            <a href="#">
-                                <img src="web_assets/img.folder/logo.jpg" alt="">
-                            </a>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua ut aliquip ex ea</p>
-                        <div class="fs-social">
-                            <a href="https://m.facebook.com/profile.php?ref=bookmarks"><i class="fa fa-facebook"></i></a>
-                            <a href="#"><i class="fa fa-twitter"></i></a>
-                            <a href="#"><i class="fa fa-youtube-play"></i></a>
-                            <a href="#"><i class="fa fa-instagram"></i></a>
-                            <a href="https://t.me/tochiscogram"><i class="fa fa-telegram"></i></a>
-                        </div>
+<!-- Footer Section Begin -->
+{{-- <footer class="footer-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="fs-about">
+                    <div class="fs-logo">
+                        <a href="#">
+                            <img src="web_assets/img.folder/logo.jpg" alt="">
+                        </a>
                     </div>
-                </div>
-                <div class="col-lg-2 col-sm-6">
-                    <div class="fs-widget">
-                        <h5>Help</h5>
-                        <ul>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Contact Support</a></li>
-                            <li><a href="#">Knowledgebase</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">FAQs</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-sm-6">
-                    <div class="fs-widget">
-                        <h5>Links</h5>
-                        <ul>
-                            <li><a href="#">Contact</a></li>
-                            <li><a href="#">Create Property</a></li>
-                            <li><a href="#">My Properties</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="fs-widget">
-                        <h5>Newsletter</h5>
-                        <p>Deserunt mollit anim id est laborum.</p>
-                        <form action="#" class="subscribe-form">
-                            <input type="text" placeholder="Email">
-                            <button type="submit" class="site-btn">Subscribe</button>
-                        </form>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua ut aliquip ex ea</p>
+                    <div class="fs-social">
+                        <a href="https://m.facebook.com/profile.php?ref=bookmarks"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-youtube-play"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="https://t.me/tochiscogram"><i class="fa fa-telegram"></i></a>
                     </div>
                 </div>
             </div>
-            <div class="copyright-text">
-                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>, distributed by <a href="https://bootthemes.com/" target="_blank">BootThemes</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            <div class="col-lg-2 col-sm-6">
+                <div class="fs-widget">
+                    <h5>Help</h5>
+                    <ul>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Contact Support</a></li>
+                        <li><a href="#">Knowledgebase</a></li>
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">FAQs</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-6">
+                <div class="fs-widget">
+                    <h5>Links</h5>
+                    <ul>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Create Property</a></li>
+                        <li><a href="#">My Properties</a></li>
+                        <li><a href="{{ route('register') }}">Register</a></li>
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="fs-widget">
+                    <h5>Newsletter</h5>
+                    <p>Deserunt mollit anim id est laborum.</p>
+                    <form action="#" class="subscribe-form">
+                        <input type="text" placeholder="Email">
+                        <button type="submit" class="site-btn">Subscribe</button>
+                    </form>
+                </div>
             </div>
         </div>
-    </footer>
-    <!-- Footer Section End -->
+        <div class="copyright-text">
+            <p>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                Copyright &copy;
+                <script>
+                    document.write(new Date().getFullYear());
+                </script> All rights reserved | This template is made with <i class="fa fa-heart"
+                    aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>, distributed
+                by <a href="https://bootthemes.com/" target="_blank">BootThemes</a>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+        </div>
+    </div>
+</footer> --}}
+<!-- Footer Section End -->
 
-    <!-- Js Plugins -->
-    @endsection
+<!-- Js Plugins -->
+@endsection
