84a1e275827becfd54c586391671a1a816cd2549
KanuPeace <kanupeace85@gmail.com>
2022-05-18 05:22:23 +0100

    footer blade

--- resources/views/web/pages/contact.blade.php
+++ resources/views/web/pages/contact.blade.php
@@ -101,65 +101,7 @@
     <!-- Contact Section End -->
 
     <!-- Footer Section Begin -->
-    <footer class="footer-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="fs-about">
-                        <div class="fs-logo">
-                            <a href="#">
-                                <img src="{{ $web_source }}/web_assets/img/m-logo.png" alt="">
-                            </a>
-                        </div>
-                      
-                        <div class="fs-social">
-                            <a href="https://m.facebook.com/profile.php?ref=bookmarks"><i class="fa fa-facebook"></i></a>
-                            <a href="#"><i class="fa fa-twitter"></i></a>
-                            <a href="#"><i class="fa fa-youtube-play"></i></a>
-                            <a href="#"><i class="fa fa-instagram"></i></a>
-                            <a href="https://t.me/tochiscogram"><i class="fa fa-telegram"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-sm-6">
-                    <div class="fs-widget">
-                        <h5>Help</h5>
-                        <ul>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Contact Support</a></li>
-                            <li><a href="#">Knowledgebase</a></li>
-                            <li><a href="#">Careers</a></li>
-                            <li><a href="#">FAQs</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-sm-6">
-                    <div class="fs-widget">
-                        <h5>Links</h5>
-                        <ul>
-                            <li><a href="#">Contact</a></li>
-                            <li><a href="#">Create Property</a></li>
-                            <li><a href="#">My Properties</a></li>
-                            <li><a href="#">Register</a></li>
-                            <li><a href="#">Login</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="fs-widget">
-                        <h5>Newsletter</h5>
-                        <p>Deserunt mollit anim id est laborum.</p>
-                        <form action="#" class="subscribe-form">
-                            <input type="text" placeholder="Email">
-                            <button type="submit" class="site-btn">Subscribe</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="copyright-text">
-              
-            </div>
-        </div>
-    </footer>
+    @include("web.pages.layouts.includes.footer")
+    <!-- Footer Section End -->
 @endsection
-<!-- Footer Section End -->
+
